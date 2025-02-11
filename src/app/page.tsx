ee4869009911663fa8d7854a77d43a50f52548ca
johannaschneidr <126594430+johannaschneidr@users.noreply.github.com>
2025-02-10 23:38:40 -0500

    bye blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Hero from '@/components/Hero'
-import Blog from '@/components/Blog'
 import RootLayout from '@/components/RootLayout'
-import { getBlogPosts } from '@/lib/mdx'
 
 export default function Home() {
-  const posts = getBlogPosts()
 
   return (
     <RootLayout>
       <Hero />
-      <Blog posts={posts} />
     </RootLayout>
   )
 }
